d0b0bc0dad77ec19dc9fbf29876e6bcee4ed8742
Nigonbol123@gmail.com <nigonbol123@gmail.com>
2022-01-14 11:50:27 -0400

    Add check functionality for task items

--- src/modules/task-functions.js
+++ src/modules/task-functions.js
@@ -36,10 +36,26 @@ const RemoveTask = (index, container) => {
 const EditTask = (data, index, e) => {
   const parent = e.target.parentElement.previousSibling;
   const description = parent.lastChild;
-  const value = prompt('coloca un valor');
+  const value = prompt('Edit selected task');
   if (value !== null && value !== '') {
     description.innerText = value;
     data[index].description = value;
+    StoreTask(JSON.stringify(data));
+  }
+};
+
+const CheckTask = (e, remove, edit, data, index) => {
+  const parent = e.target.parentElement;
+  const check = parent.firstChild;
+  if (check.checked) {
+    remove[index].classList.toggle('toggle');
+    edit[index].classList.toggle('toggle');
+    data[index].check = true;
+    StoreTask(JSON.stringify(data));
+  } else {
+    remove[index].classList.toggle('toggle');
+    edit[index].classList.toggle('toggle');
+    data[index].check = false;
     StoreTask(JSON.stringify(data));
   }
 };
@@ -52,19 +68,26 @@ const DisplayTask = (container, data) => {
     data.map((e, i) => {
       newLiTag += `<li class="task"><div class="item-description"><input type="checkbox" name="check" class="check"><p class="description">${data[i].description}</p></div><div class="list-icons"><i class="fas fa-trash remove"></i><i class="fas fa-pen edit"></i></div></li>`;
       container.innerHTML = newLiTag;
-      const removeBtn = document.querySelectorAll('i.remove');
-      removeBtn.forEach((e, index) => {
-        removeBtn[index].addEventListener('click', (e) => {
-          e.target.parentElement.parentElement.remove();
-          RemoveTask(index, container);
-        });
-      });
       return data;
+    });
+    const removeBtn = document.querySelectorAll('i.remove');
+    removeBtn.forEach((e, index) => {
+      removeBtn[index].classList.toggle('toggle');
+      removeBtn[index].addEventListener('click', (e) => {
+        e.target.parentElement.parentElement.remove();
+        RemoveTask(index, container);
+      });
     });
     const editBtn = document.querySelectorAll('i.edit');
     editBtn.forEach((e, index) => {
       editBtn[index].addEventListener('click', (e) => {
         EditTask(data, index, e);
+      });
+    });
+    const checkBtn = document.querySelectorAll('.check');
+    checkBtn.forEach((e, index) => {
+      checkBtn[index].addEventListener('change', (e) => {
+        CheckTask(e, removeBtn, editBtn, data, index);
       });
     });
   }
